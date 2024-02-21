80180a2afd8617f7f745ca984af187f0acf274f0
Rudney <rudneyngueko@gmail.com>
2024-02-21 07:03:21 +0300

    first commit

---
+++ src/test/java/com/dedytech/demo_minio/DemoMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedytech.demo_minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
